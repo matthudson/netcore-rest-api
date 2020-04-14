b5d806489bfe5567cb2a5f700a26dffd54655b2c
Matthew Hudson <m@tthudson.com>
2020-04-14 03:59:26 +0100

    Dto & DataAnnotation Added

---
+++ NetCoreRestApi/NetCoreRestApi.Auth/DataAnnotations/DateMustBe18YearsOldAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreRestApi.Auth.DataAnnotations
+{
+    public class DateMustBe18YearsOldAttribute : RangeAttribute
+    {
+        public DateMustBe18YearsOldAttribute()
+            : base(typeof(DateTime), DateTime.Now.AddYears(-18).ToShortDateString(), DateTime.Now.AddYears(120).ToShortDateString()) { }
+    }
+}
---
+++ NetCoreRestApi/NetCoreRestApi.Auth/Dtos/CustomerRegistrationDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using NetCoreRestApi.Auth.DataAnnotations;
+
+namespace NetCoreRestApi.Auth.Dtos
+{
+    public class CustomerRegistrationDto
+    {
+        // Required, 3-50
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Forename { get; set; }
+
+        // Required, 3-50
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Surname { get; set; }
+
+        // Optional; Validate 4+[alphanum]@2+[alphanum].co(m/.uk)
+        [RegularExpression("^[a-zA-Z0-9]{4,}@[a-zA-Z0-9]{2,}\\.co(m|\\.uk)$")]
+        public string Email { get; set; }
+
+        // Optional; Validate >= 18yrs
+        [DateMustBe18YearsOld]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
